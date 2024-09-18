d4756085da03eec112f059c8bd582c7e8de43fb0
Macbook <macbook@MacBooks-MacBook-Pro.local>
2024-09-18 22:32:29 +0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go-web-native-crud/config"
+	"go-web-native-crud/controllers/categorycontroller"
+	"go-web-native-crud/controllers/homecontroller"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.ConnectDB()
+
+	//kita panggil 1. Homepage
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	//kita panggil 2. Categories
+	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	log.Println("Serve running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
